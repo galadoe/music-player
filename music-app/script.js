88a7d9fa5f123f099d74339b990982b76b7d3885
galadoe <kgaladoe@gmail.com>
2021-08-13 00:36:49 -0700

    adding the functionality

---
+++ music-app/script.js
@@ -0,0 +1,14 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const previousBtn = document.querySelector('#previous')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+//Song titles
+const songs = ['Permission to Dance', 'Shes in the Rain', 'Industry Baby', 'Truth Untold', 'Face', 'Gods Menu']
+
+//
