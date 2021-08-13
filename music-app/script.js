20a72c51109206bdc5edb38f2813088b743e1dd0
galadoe <kgaladoe@gmail.com>
2021-08-13 01:31:57 -0700

    trying to fix some bugs

--- music-app/script.js
+++ music-app/script.js
@@ -1,6 +1,6 @@
 const musicContainer = document.querySelector('.music-container')
 const playBtn = document.querySelector('#play')
-const previousBtn = document.querySelector('#previous')
+const prevBtn = document.querySelector('#previous')
 const nextBtn = document.querySelector('#next')
 const audio = document.querySelector('#audio')
 const progress = document.querySelector('.progress')
@@ -9,6 +9,90 @@ const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
 
 //Song titles
-const songs = ['Permission to Dance', 'Shes in the Rain', 'Industry Baby', 'Truth Untold', 'Face', 'Gods Menu']
+const songs = ['bts-permission-to-dance', 'the-rose-rain', 'lil-nas-baby', 'bts-untold', 'face-woosung', 'skz-gods-menu']
 
-//
+//keep track of songs
+let songIndex = 0
+
+//Initially load song info DOM
+loadSong(songs[songIndex])
+
+//Update song details
+function loadSong(song) {
+  title.innerText = song
+  audio.src = `music/${song}.mp3`
+  cover.src = `images/${song}.jpg`
+}
+
+function playSong() {
+  musicContainer.classList.add('play')
+  playBtn.querySelector('i.fas').classList.remove('fa-play')
+  playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+  audio.play()
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play')
+  playBtn.querySelector('i.fas').classList.add('fa-play')
+  playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+  audio.pause()
+}
+
+function prevSong() {
+  songIndex--
+
+  if(songIndex < 0){
+    songIndex = songs.length - 1
+  }
+
+  loadSong(songs[songIndex])
+
+  playSong()
+}
+
+function nextSong() {
+  songIndex++
+
+  if(songIndex > songs.length - 1){
+    songIndex = 0
+  }
+
+  loadSong(songs[songIndex])
+
+  playSong()
+}
+
+function updateProgress(e) {
+  const {duration, currentTime} = e.srcElement.currentTime
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX / width) * duration
+}
+
+//Event Listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play')
+
+  if(isPlaying) {
+    pauseSong()
+  }else {
+    playSong()
+  }
+})
+
+//change song events
+prevBtn.addEventListener('click', prevSong)
+prevBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
